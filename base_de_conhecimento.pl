afe2a188cd8d6a36401fdc4323d68a616ef08146
mariajcpereira <59666241+mariajcpereira@users.noreply.github.com>
2021-04-15 14:34:25 +0100

    Update base_de_conhecimento.pl

--- base_de_conhecimento.pl
+++ base_de_conhecimento.pl
@@ -1,1 +1,26 @@
+%Perfis
 
+if Portuguesa and    then 1.
+if   and    then 2.
+if   and    then 3.
+if   and    then 4.
+if   and    then 5.
+if   and    then 6.
+if   and    then 7.
+if   and    then 8.
+if   and    then 9.
+if   and    then 10.
+if   and    then 11.
+if   and    then 12.
+if   and    then 13.
+if   and    then 14.
+if   and    then 15.
+if   and    then 16.
+if   and    then 17.
+if   and    then 18.
+if   and    then 19.
+if   and    then 20.
+if   and    then 21.
+if   and    then 22.
+if   and    then 23.
+if   and    then 24.
