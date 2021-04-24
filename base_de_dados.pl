e027ed3429db06792e0ae389d7244784c3fda58b
mariajcpereira <59666241+mariajcpereira@users.noreply.github.com>
2021-04-24 17:20:31 +0100

    Update base_de_dados.pl

--- base_de_dados.pl
+++ base_de_dados.pl
@@ -8,7 +8,7 @@ restaurante('Bira dos Namorados',[hamburgueres], braga, muito_bom, acessivel, ta
 restaurante('BLB',[sanduiches], braga, muito_bom, acessivel, takeaway_nao, entrega_sim, medio).
 restaurante('Bracara Augusta',[portuguesa], braga, muito_bom, medio, takeaway_nao, entrega_sim, medio).
 restaurante('Braga999',[japonesa, sushi], braga, muito_bom, acessivel, takeaway_sim, entrega_sim, medio).
-restaurante('BurgerKing',[hamburgueres], braga, muito_bom, acessivel, takeaway_sim, entrega_sim, medio).
+restaurante('BurgerKing',[hamburgueres], braga, muito_bom, acessivel, takeaway_sim, entrega_sim, gratuito).
 restaurante('Formigas', [portuguesa, sushi, japonesa], guimaraes, muito_bom, medio, takeaway_sim, entrega_sim, medio).
 restaurante('Cafe do Luis',[hamburgueres, sanduiches],braga, muito_bom, acessivel, takeaway_sim, entrega_sim, medio).
 restaurante('Cafe Vianna',[portuguesa], braga, muito_bom, medio, takeaway_sim, entrega_sim, medio).
@@ -42,22 +42,22 @@ restaurante('Oubela',[portugesa, pizza], braga, acessivel, takeaway_sim, entrega
 restaurante('Pastelaria JP',[portugesa], braga, acessivel, takeaway_sim, entrega_sim, baixo).
 restaurante('Pause Café',[sanduiches], braga, acessivel, takeaway_sim, entrega_sim, medio).
 restaurante('Pizza di Mestre',[italiana, pizza], braga, acessivel, takeaway_sim, entrega_sim, baixo).
-restaurante('Pizza Hut',[pizza],'braga', 'medio', sim, entrega_sim, medio).
-restaurante('Rotações',[pizza],'braga', 'acessivel', sim,[glovo], medio).
-restaurante('Sandes Braga',[sanduiches],'braga', 'acessivel', sim,[glovo], medio).
-restaurante('SANNO',[saudavel],'braga', 'acessivel', sim, [ubereats, glovo], medio).
-restaurante('Semente Art Coffee & Plant Based',[saudavel, hamburgueres],'braga', 'acessivel', nao, [ubereats], medio).
-restaurante('Shakai',[sushi, japonesa],'braga', 'acessivel', sim, [ubereats], medio).
-restaurante('Sushi em tua casa',[sushi, japonesa],'braga', 'medio', sim, [ubereats], medio).
-restaurante('Sushi&Co',[sushi, japonesa],'braga', 'medio', sim, [ubereats, glovo], medio).
-restaurante('Taberna Londrina',[portuguesa],'guimaraes', 'medio', sim,[ubereats, glovo], medio).
-restaurante('Taquería El Cascabel',[mexicana],'braga', 'acessivel', nao, [ubereats], medio).
-restaurante('Telepizza',[pizza],'braga', 'acessivel', nao, [ubereats, glovo], medio).
-restaurante('Temple San',[sushi, japonesa],'braga', 'medio', sim,[ubereats], medio).
-restaurante('Tia Isabel',[portuguesa],'braga', 'medio', sim, [ubereats, glovo], medio).
-restaurante('Tourigalo',[portuguesa, churrasco],'braga', 'medio', sim,[ubereats, glovo], medio).
-restaurante('ViaBracara',[portuguesa],'braga', 'acessivel', nao, [ubereats, glovo], medio).
-restaurante('Cachorrão',[hamburgueres, sanduiches],'guimaraes', 'acessivel', nao, [ubereats], medio).
+restaurante('Pizza Hut',[pizza], braga, medio, takeaway_sim, entrega_sim, medio).
+restaurante('Rotações',[pizza], braga, acessivel, takeaway_sim, entrega_sim, medio).
+restaurante('Sandes Braga',[sanduiches], braga, acessivel, takeaway_sim, entrega_sim, medio).
+restaurante('SANNO',[saudavel], braga, acessivel, sim, [ubereats, glovo], medio).
+restaurante('Semente Art Coffee & Plant Based',[saudavel, hamburgueres], braga, acessivel, takeaway_nao, entrega_sim, medio).
+restaurante('Shakai',[sushi, japonesa],braga, acessivel,entrega_sim, entrega_sim, medio).
+restaurante('Sushi em tua casa',[sushi, japonesa], braga, medio, takeaway_sim, entrega_sim, medio).
+restaurante('Sushi&Co',[sushi, japonesa], braga, medio, takeaway_sim, entrega_sim, medio).
+restaurante('Taberna Londrina',[portuguesa], guimaraes, medio, takeaway_sim, entrega_sim, medio).
+restaurante('Taquería El Cascabel',[mexicana], braga, acessivel, takeaway_nao, entrega_sim, medio).
+restaurante('Telepizza',[pizza], braga, acessivel, takeaway_nao, entrega_sim, gratuito).
+restaurante('Temple San',[sushi, japonesa], braga, medio, takeaway_sim, entrega_sim, medio).
+restaurante('Tia Isabel',[portuguesa], braga, medio, takeaway_sim, entrega_sim, medio).
+restaurante('Tourigalo',[portuguesa, churrasco], braga, medio, takeaway_sim, entrega_sim, medio).
+restaurante('ViaBracara',[portuguesa], braga, acessivel, takeaway_nao, entrega_sim, medio).
+restaurante('Cachorrão',[hamburgueres, sanduiches], guimaraes, acessivel, takeaway_nao, entrega_sim, medio).
 
 
 membro( X, [X|_] ).
