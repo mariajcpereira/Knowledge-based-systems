7af48a028b4e8c9f00d6538b046528fce6186eba
mariajcpereira <59666241+mariajcpereira@users.noreply.github.com>
2021-04-24 17:22:11 +0100

    Update base_de_conhecimento.pl

--- base_de_conhecimento.pl
+++ base_de_conhecimento.pl
@@ -1,17 +1,17 @@
 %Perfis
 
-if guimaraes and sim then 1.
+if guimaraes and takeaway_sim then 1.
 if acessivel and gratuito then 2.
-if sushi and braga and sim then 3.
+if sushi and braga and takeaway_sim then 3.
 if hamburgueres and guimaraes then 4.
-if italiana and braga and sim then 5.
-if pizza and guimaraes and sim then 6.
-if japonesa and ubereats and medio then 7.
-if braga and acessivel and ubereats then 8.
-if mexicana and ubereats and medio then 9.
+if italiana and braga and takeaway_sim then 5.
+if pizza and guimaraes and takeaway_sim then 6.
+if japonesa and entrega_sim and medio then 7.
+if braga and acessivel and entrega_sim then 8.
+if mexicana and entrega_sim and medio then 9.
 if hamburgueres and acessivel and sim then 10.
-if churrasco and braga and glovo and medio then 11.
+if churrasco and braga and entrega_sim and medio then 11.
 if hamburgueres and braga and acessivel and medio then 12.
-if portuguesa and braga and medio and glovo and medio then 13.
-if portuguesa and guimaraes and sim and ubereats and medio then 14.
-if saudavel and braga and acessível and sim and ubereats and medio then 15.
+if portuguesa and braga and medio and entrega_sim and medio then 13.
+if portuguesa and guimaraes and sim and entrega_sim and medio then 14.
+if saudavel and braga and acessível and sim and entrega_sim and medio then 15.
