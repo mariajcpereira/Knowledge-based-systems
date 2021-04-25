2651a9e194d33be5e16ee717d0034a39bda8188c
mariajcpereira <59666241+mariajcpereira@users.noreply.github.com>
2021-04-25 16:25:20 +0100

    Update baseconhecimento.pl

--- baseconhecimento.pl
+++ baseconhecimento.pl
@@ -1,17 +1,17 @@
 %Perfis
 
 if guimaraes and takeaway_sim then 1.
-if acessivel and gratuito then 2.
+if acessivel and custo_nao then 2.
 if sushi and braga and takeaway_sim then 3.
 if hamburgueres and guimaraes then 4.
 if italiana and braga and takeaway_sim then 5.
 if pizza and guimaraes and takeaway_sim then 6.
-if japonesa and entrega_sim and medio then 7.
+if japonesa and entrega_sim and custo_sim then 7.
 if braga and acessivel and entrega_sim then 8.
-if mexicana and entrega_sim and medio then 9.
-if hamburgueres and acessivel and sim then 10.
-if churrasco and braga and entrega_sim and medio then 11.
-if hamburgueres and braga and acessivel and medio then 12.
-if portuguesa and braga and medio and entrega_sim and medio then 13.
-if portuguesa and guimaraes and entrega_sim and medio then 14.
-if saudavel and braga and acessível and entrega_sim and medio then 15.
+if mexicana and entrega_sim and custo_sim then 9.
+if hamburgueres and acessivel and takeaway_sim then 10.
+if churrasco and braga and entrega_sim and custo_sim then 11.
+if hamburgueres and braga and acessivel and custo_nao then 12.
+if portuguesa and braga and medio and entrega_sim and custo_nao then 13.
+if portuguesa and guimaraes and entrega_sim and custo_sim then 14.
+if saudavel and braga and acessível and entrega_sim and custo_nao then 15.
