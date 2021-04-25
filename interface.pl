48acc6a50ff0d01deae1044530454f2e646c53ed
mariajcpereira <59666241+mariajcpereira@users.noreply.github.com>
2021-04-25 15:35:40 +0100

    Update interface.pl

--- interface.pl
+++ interface.pl
@@ -23,22 +23,42 @@ avaliarEscolha(1):- questao1.
 avaliarEscolha(2):- write('Foi um prazer ajuda-lo!'), halt.
 avaliarEscolha(other):- write('Introduza uma opcao valida, por favor comece de novo'), menu.
 
+	    
 questao1:-	write('********************************************************************************************************'), nl,
+			write('**  Deseja um restaurante de Braga ou Guimaraes?'), nl,
+			write('**'),nl,
+			write('**  1 - Braga'), nl,
+			write('**  2 - Guimaraes'), nl,nl,
+			read(A1),
+			(
+			(A1 == 1),assert(fact(braga)), questao2;
+            (A1 == 2),assert(fact(guimaraes)), questao2.
+	    
+questao2:-	write('********************************************************************************************************'), nl,
 			write('**  Gostaria de levantar o pedido ou que seja entregue a sua casa?'), nl,
 			write('**'),nl,
 			write('**  1 - TakeAway'), nl,
 			write('**  2 - Entrega'), nl,nl,
-			read(A1),
+			read(A2),
 			(
-			(A1 == 1),assert(fact(take_away)), questao3;
-            (A1 == 2),assert(fact(entrega)), questao2).
-
-questao2:-	write('********************************************************************************************************'), nl,
-			write('**  Qual o nome do ator/atriz?'), nl,
-			write('**'),nl,nl,
-			read(A4),assert(variavel(A4)),resultado.
-			
+			(A2 == 1),assert(fact(takeaway_sim)), questao4;
+            (A2 == 2),assert(fact(entrega_sim)), questao3.
+	    
 questao3:-	write('********************************************************************************************************'), nl,
+			write('**  Que taxa de entrega está disposto a pagar?'), nl,
+			write('**'),nl,
+			write('**  1 - Gratuito'), nl,
+			write('**  2 - Baixo'), nl,
+			write('**  3 - Médio'), nl,
+			write('**  4 - Indiferente'), nl,nl,
+			read(A3),
+			(
+			(A3 == 1),assert(fact(gratuito)), questao4;
+	    (A3 == 2),assert(fact(baixo)), questao4;
+	    (A3 == 3),assert(fact(medio)), questao4;
+            (A3 == 4), questao4.
+			
+questao4:-	write('********************************************************************************************************'), nl,
 			write('**  Que tipo de alimentacao deseja? Destas opcoes, introduza o numero correspondente:'), nl,
 			write('**'),nl,
 			write('**  1 - Portuguesa'), nl,
@@ -51,36 +71,31 @@ questao3:-	write('**************************************************************
 			write('**  8 - Italiana'), nl,
 			write('**  9 - Saudavel'), nl,
 			write('**  10 - Mexicana'), nl,
-			read(A3),
+			read(A4),
 			(
-			(A3 == 1), assert(fact(portuguesa)), questao4;
-            (A3 == 2), assert(fact(churrasco)), questao4;
-            (A3 == 3), assert(fact(hamburgueres)), questao4;
-            (A3 == 4), assert(fact(sanduiches)), questao4;
-            (A3 == 5), assert(fact(japonesa)), questao4;
-            (A3 == 6), assert(fact(sushi)), questao4;
-            (A3 == 7), assert(fact(pizza)), questao4;
-            (A3 == 8), assert(fact(italiana)), questao4;
-            (A3 == 9), assert(fact(saudavel)), questao4;
-            (A3 == 10), assert(fact(mexicana)), questao4.
-			
-
-questao4:- 	write('********************************************************************************************************'), nl,
-			write('**  '), nl, 
-			write('**'),nl,nl,			
-			read(A4), assert(variavel(A4)),
-			questao5.
+			(A4 == 1), assert(fact(portuguesa)), questao5;
+            (A4 == 2), assert(fact(churrasco)), questao5;
+            (A4 == 3), assert(fact(hamburgueres)), questao5;
+            (A4 == 4), assert(fact(sanduiches)), questao5;
+            (A4 == 5), assert(fact(japonesa)), questao5;
+            (A4 == 6), assert(fact(sushi)), questao5;
+            (A4 == 7), assert(fact(pizza)), questao5;
+            (A4 == 8), assert(fact(italiana)), questao5;
+            (A4 == 9), assert(fact(saudavel)), questao5;
+            (A4 == 10), assert(fact(mexicana)), questao5.
 			
 
 questao5:- 	write('********************************************************************************************************'), nl,
-			write('**  E maior de idade?'), nl,
+			write('**  Deseja um restaurante com que preco de refeicao?'), nl,
 			write('**'),nl,
-			write('**  1 - Sim, tenho mais de 18 anos'),nl, 
-			write('**  2 - Nao, tenho menos de 18 anos' ),nl, nl,
+			write('**  1 - Preco acessivel'),nl, 
+			write('**  2 - Preco medio'),nl, 
+			write('**  3 - Preco elevado'),n1,nl,
 			read(A5),nl,
 		    (
-		    (A5 == 1), assert(fact(maior18)), resultado;
-			(A5 == 2), assert(fact(menor18)), resultado).
+		    (A5 == 1), assert(fact(acessivel)), resultado;
+		    (A5 == 2), assert(fact(medio)), resultado;
+			(A5 == 3), assert(fact(caro)), resultado).
 			
 			
 resultado :- 	write('********************************************************************************************************'), nl,
@@ -90,9 +105,9 @@ resultado :- 	write('***********************************************************
 				write('********************************************************************************************************'),
 				result.
 
-resultadowrite(P):-	variavel(A4),nl,
+/*resultadowrite(P):-	variavel(A4),nl,
 					write('     O seu perfil e o'),nl,
 					write('     *** '),write(P),write(' ***'),nl,nl,
-					write('     Filmes aconselhados: '),perfil(P,A4),nl,nl,
+					write('     Restaurantes aconselhados: '),perfil(P,A4),nl,nl,
 					write('********************************************************************************************************'),
-					retract(variavel(A4)), retractall(fact(_)).
+					retract(variavel(A4)), retractall(fact(_)).*/
