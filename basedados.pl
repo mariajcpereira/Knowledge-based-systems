29c766c9b84a79eebc2cfbf520260ff3adbca7b0
mariajcpereira <59666241+mariajcpereira@users.noreply.github.com>
2021-04-28 11:54:37 +0100

    Update basedados.pl

--- basedados.pl
+++ basedados.pl
@@ -87,9 +87,9 @@ perfil(1):- verrefeicao(_,guimaraes,_,takeaway_sim,_,_).
 perfil(2):- verrefeicao(_,_,acessivel,_,_,custo_nao).
 perfil(3):- verrefeicao(sushi,braga,_,takeaway_sim,_,_).
 perfil(4):- verrefeicao(hamburgueres,guimaraes,_,_,_,_).
-perfil(5):- verrefeicao(italiana,braga,_,takeaway_sim,_._).
+perfil(5):- verrefeicao(italiana,braga,_,takeaway_sim,_,_).
 perfil(6):- verrefeicao(pizza,guimaraes,_,takeaway_sim,_,_).
-perfil(7):- verrefeicao(japonesa,_,_,entrega_sim,custo_sim).
+perfil(7):- verrefeicao(japonesa,_,_,_,entrega_sim,custo_sim).
 perfil(8):- verrefeicao(_,braga,acessivel,_,entrega_sim,_).
 perfil(9):- verrefeicao(mexicana,_,_,_,entrega_sim,custo_sim).
 perfil(10):- verrefeicao(hamburgueres,_,acessivel,takeaway_sim,_,_).
