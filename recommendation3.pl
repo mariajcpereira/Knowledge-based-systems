b9db935b0e842a250abcb8ecbe9ce907e5c2d671
mariajcpereira <59666241+mariajcpereira@users.noreply.github.com>
2021-04-28 12:21:09 +0100

    Create recommendation3.pl

---
+++ recommendation3.pl
@@ -0,0 +1,15 @@
+:-[satisfy,induce_ifthen,recommendation2].
+
+% the goal is to classify recommendation
+learn_rules:-
+	learn(ma),  % negative examples
+	learn(boa), % positive examples
+        % save new rules:
+	tell('recommendationifthen.pl'),
+	listing(<==),
+	told.
+
+% example of classifying `new' objects:
+q1(Class):- classify([outlook=sunny,temperature=hot,humidity=normal,wind=weak],Class).
+q2(Class):- classify([outlook=sunny,humidity=high],Class).
+q3(Class):- classify([outlook=rain,temperature=mild,humidity=normal,wind=strong],Class).
