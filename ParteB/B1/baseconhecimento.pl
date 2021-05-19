2903a7ca3115e208f5bd2d07fd675c8c244fbf6a
mariajcpereira <59666241+mariajcpereira@users.noreply.github.com>
2021-05-19 11:26:55 +0100

    Update baseconhecimento.pl

--- ParteB/B1/baseconhecimento.pl
+++ ParteB/B1/baseconhecimento.pl
@@ -3,6 +3,21 @@
 %:- dynamic /2 .
 :-[basedados].
 
+%ocupacaoEncomenda
+
+ocupacaoEncomenda([],0).
+ocupacaoEncomenda([X|R],Soma) :- passageiros(X,TotalParagem), 
+ocupacaoEncomenda(R,TotalEncomenda), Soma is TotalParagem + TotalEncomenda.
+
+guardarOcupacaoEncomenda(Caminho):- ocupacaoEncomenda(Caminho, Valor), 
+assertz(ocupacao(Caminho, Valor)).
+
+gerarOcupacoesCaminhos([]).
+gerarOcupacoesCaminhos([C1|R1]):- guardarOcupacaoEncomenda(C1), gerarOcupacoesCaminhos(R1).
+
+
+
+
 %DistanciaCaminho
 calcularDistancia(_,[],0).
 calcularDistancia(P1,[P2|R2], Soma):- percurso(P2,P1,Distancia), 
@@ -17,107 +32,11 @@ gerarDistanciasCaminhos([C1|R1]):- guardarDistancia(C1), gerarDistanciasCaminhos
 distanciaCaminho([P1|R1], Valor):-  calcularDistancia(P1,R1,Valor).
 
 
+
+
+
 %GerarListaCaminhos
 listaCaminhos(X,Y,Lista):- findall(C,caminho(X,Y,C),Lista).
 
-%TempoCaminhoMinALTERAR
-
-calcularTempoMin(Caminho):- guardarDistancia(Caminho), guardarOcupacaoAutocarro(Caminho), 
-ocupacao(Caminho, OcupacaoCaminho), comprimento(Caminho, DistanciaCaminho), 
-assertz(passageiroKM(Caminho, OcupacaoCaminho / DistanciaCaminho)).
-
-gerarPassageirosKM([]).
-gerarPassageirosKM([C1|R1]):- calcularPassageirosKM(C1), gerarPassageirosKM(R1).
-
-passageiroKMCaminho(Caminho, Indice):- guardarDistancia(Caminho), guardarOcupacaoAutocarro(Caminho), 
-ocupacao(Caminho, OcupacaoCaminho), comprimento(Caminho, DistanciaCaminho), Indice is OcupacaoCaminho / DistanciaCaminho.
 
 
-
-%QuestoesDoUtilizador
-
-%CaminhoMaisCurto
-caminhoMaisCurto(X,Y,Caminho):- retractall(comprimento(_,_)), listaCaminhos(X,Y,Lista), 
-gerarDistanciasCaminhos(Lista), maisCurto(Caminho).
-
-maisCurto(Caminho):- comprimento(Caminho, V), \+ (comprimento(_, V1), V > V1).
-
-
-%CaminhoMaisLongo
-caminhoMaisLongo(X,Y,Caminho):- retractall(comprimento(_,_)), listaCaminhos(X,Y,Lista), 
-gerarDistanciasCaminhos(Lista), maisLongo(Caminho).
-
-maisLongo(Caminho):- comprimento(Caminho, V), \+ (comprimento(_, V1), V1 > V).
-
-
-
-
-
-
-%ocupacaoEncomenda
-
-ocupacaoEncomenda([],0).
-ocupacaoEncomenda([X|R],Soma) :- passageiros(X,TotalEstacao), 
-ocupacaoEncomenda(R,TotalAutocarro), Soma is TotalEstacao + TotalAutocarro.
-
-guardarOcupacaoAutocarro(Caminho):- ocupacaoAutocarro(Caminho, Valor), 
-assertz(ocupacao(Caminho, Valor)).
-
-gerarOcupacoesCaminhos([]).
-gerarOcupacoesCaminhos([C1|R1]):- guardarOcupacaoAutocarro(C1), gerarOcupacoesCaminhos(R1).
-
-
-
-
-
-
-
-
-
-%CaminhosDisponiveisALTERAR
-
-caminho(X,Z,Caminho):- caminho(X,Z,[X],Caminho).
-caminho(X,X,Caminho,Caminho):- ocupacaoEncomenda(Caminho,Soma), Soma == 1.
-caminho(X,Z,Visitado,Caminho):- percurso(X,Y,_), \+ member(Y,Visitado), 
-caminho(Y,Z,[Y | Visitado],Caminho).
-
-
-
-
-
-%GerarListaCaminhosFEITO
-listaCaminhos(X,Y,Lista):- findall(C,caminho(X,Y,C),Lista).
-
-%QuestoesDoUtilizador
-
-%CaminhoMaisCurtoFEITO
-caminhoMaisCurto(X,Y,Caminho):- retractall(comprimento(_,_)), listaCaminhos(X,Y,Lista), 
-gerarDistanciasCaminhos(Lista), maisCurto(Caminho).
-
-maisCurto(Caminho):- comprimento(Caminho, V), \+ (comprimento(_, V1), V > V1).
-
-
-%CaminhoMaisLongoFEITO
-caminhoMaisLongo(X,Y,Caminho):- retractall(comprimento(_,_)), listaCaminhos(X,Y,Lista), 
-gerarDistanciasCaminhos(Lista), maisLongo(Caminho).
-
-maisLongo(Caminho):- comprimento(Caminho, V), \+ (comprimento(_, V1), V1 > V).
-
-
-%CaminhoMenosLucroALTERAR
-caminhoMenosPassageiros(X,Y,Caminho):- retractall(ocupacao(_,_)), listaCaminhos(X,Y,Lista), 
-gerarOcupacoesCaminhos(Lista), menosPassageiros(Caminho).
-
-menosPassageiros(Caminho):- ocupacao(Caminho, V), \+ (ocupacao(_, V1), V > V1).
-
-%CaminhoMaisLucroALTERAR
-caminhoMaisPassageiros(X,Y,Caminho):- retractall(ocupacao(_,_)), listaCaminhos(X,Y,Lista), 
-gerarOcupacoesCaminhos(Lista), maisPassageiros(Caminho).
-
-maisPassageiros(Caminho):- ocupacao(Caminho, V), \+ (ocupacao(_, V1), V1 > V).
-
-%CaminhoMaisVantajosoALTERAR
-caminhoMaisVantajoso(X,Y,Caminho):- retractall(passageiroKM(_,_)), listaCaminhos(X,Y,Lista), 
-gerarPassageirosKM(Lista),!, maisVantajoso(Caminho).
-
-maisVantajoso(Caminho):- passageiroKM(Caminho, V), \+ (passageiroKM(_, V1), V1 > V).
