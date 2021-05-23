2becc402a9599ca8561c23aaa1f49b54fa1c4054
mariajcpereira <59666241+mariajcpereira@users.noreply.github.com>
2021-05-23 16:53:39 +0100

    Update interface.pl

--- ParteB/B1/interface.pl
+++ ParteB/B1/interface.pl
@@ -17,24 +17,6 @@ opcao(other):- write(''), inicio.
 
 
 comecar:-	write('********************************************************************************************************'), nl,
-			write('**  Qual dos clientes pretende satisfazer com esta entrega?'), nl,
-			write('**'),nl,
-			write('**  1 - Cliente 1'), nl,
-			write('**  2 - Cliente 2'), nl,
-			write('**  3 - Cliente 3'), nl,
-			write('**  4 - Cliente 4'), nl,
-			write('**  5 - Cliente 5'), nl,nl,
-			read(C),
-			(
-			(C == 1), assert(goal(client1)), tipoProcura;
-            (C == 2), assert(goal(cliente2)), tipoProcura;
-			(C == 3), assert(goal(cliente3)), tipoProcura;
-			(C == 4), assert(goal(cliente4)), tipoProcura;
-			(C == 5), assert(goal(cliente5)), tipoProcura).
-
-%run(Method):- search(Method,Par,Solution).			
-
-tipoProcura:-	write('********************************************************************************************************'), nl,
 			write('**  Que tipo de procura deseja aplicar nesta entrega?'), nl,
 			write('**'),nl,
 			write('**  1 - depthfirst'), nl,
@@ -45,3 +27,6 @@ tipoProcura:-	write('***********************************************************
 			(P == 1), run(depthfirst);
             (P == 2), run(iterativedeepening);
 			(P == 3), run(breadthfirst)).
+			
+
+
