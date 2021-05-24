3a8b7be2c63284dcdea9e7dbd9c89715efee2845
mariajcpereira <59666241+mariajcpereira@users.noreply.github.com>
2021-05-24 13:23:12 +0100

    Update recommendation3.pl

--- ParteA/A2/recommendation3.pl
+++ ParteA/A2/recommendation3.pl
@@ -14,6 +14,8 @@ list([categoria=portuguesa,custo_medio=acessivel,takeaway=nao,entrega=sim, custo
 
 q1:- list(L), classify(L,Class), write(Class).
 
+q2:- classify([categoria=portuguesa,custo_medio=acessivel,takeaway=nao,entrega=sim, custo_entrega=sim], X), write(X).
+
 adiciona(X, L, [X|L]).
 
 add(A):- lista(L), adiciona(A, L, L2), retract(lista(L)), assert(lista(L2)).
