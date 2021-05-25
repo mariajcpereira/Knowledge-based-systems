3ee96e5c7839dea964c5c6acfa449390b150da4f
mariajcpereira <59666241+mariajcpereira@users.noreply.github.com>
2021-05-25 23:36:19 +0100

    Update percurso1.pl

--- ParteB/B1/percurso1.pl
+++ ParteB/B1/percurso1.pl
@@ -44,7 +44,7 @@ eval([Cliente1,Cliente2|R],DS):-
               write('Caminho: '), writePercurso(S),nl,
               %length(S,N),N1 is N-1,write('Tamanho:'),write(N1),nl,
               write('Tempo: '), writeTempo(Q), nl,
-              write('Lucro: '), writeLucro(Q),nl, fail.
+              write('Lucro: '), entrega(Q),nl, fail.
 
 	      
 writeTempo(sol(_,LC)):- eval(LC,D), D2 is D+1,write(D2).
@@ -53,7 +53,7 @@ writeEntrega(sol(LE,_)):- write(LE).
 
 writePercurso(sol(_,LC)):- write(LC).
 
-writeLucro(sol(LE,_)):- member(A,LE),
+entrega(sol(LE,_)):- member(A,LE),
 entrega(A,Y), write(Y).	      
 	      
 	      
