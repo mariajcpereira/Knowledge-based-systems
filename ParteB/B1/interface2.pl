65e3ec8f1584d6eed3cdea6a78d5aa1c1cffce10
mariajcpereira <59666241+mariajcpereira@users.noreply.github.com>
2021-05-26 01:17:56 +0100

    Update interface2.pl

--- ParteB/B1/interface2.pl
+++ ParteB/B1/interface2.pl
@@ -1,6 +1,8 @@
 :-[search, percurso2].
 
-inicio:- nl,nl ,write('*********************************************************************************************************'), nl,
+:- use_module(library(theme/dark)).
+
+inicio:- nl,nl ,write('********************************************************************************************************+******************'), nl,
 				write('                                                                                                          '), nl,			 
 				write('                   ACONSELHAMENTO DE TRAJETO PARA ENTREGA DE UMA REFEICAO 				'), nl,              
 				write('                                                                                                          '), nl,			 
